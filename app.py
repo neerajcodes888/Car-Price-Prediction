d4098d6a25515116609e43fc1ffb31906f3b1e41
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-03 12:49:03 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+
+
+app = Flask(__name__)
+
+model  = joblib.load('Prediction_Model')
+
+@app.route('/')
+def home():
+    return render_template("index.html")
